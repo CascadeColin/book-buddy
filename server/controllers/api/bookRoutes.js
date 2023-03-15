30821c05cae39487aa2f49c0526508566c59da04
Colin Marshall <colin.marshall94@gmail.com>
2023-03-15 14:46:19 -0700

    found a better api call that gets us descriptions

--- server/controllers/api/bookRoutes.js
+++ server/controllers/api/bookRoutes.js
@@ -27,7 +27,7 @@ router.get("/book", async (req,res) {
         const isbnArray = await fetch(`https://openlibrary.org/search.json?title=${searchTitle}`)
         // hardcode isbnArray[0] to be used - we can get fancy later
         const isbn = access isbnArray[0] object and get string value of ISBN
-        const bookDetails = await fetch(`https://openlibrary.org/books/${isbn}.json`)
+        const bookDetails = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`)
         *access 'bookDetails' and send the data to DB.  DB will store data, Apollo *will be used to connect DB to front-end to render data
     } catch (err) {
         console.error(err)
