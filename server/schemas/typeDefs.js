7a39cd8ddb7eb666c1af4fb9e70c1e31ec7e408e
Kim Van Wyk <kmvanwyk@msn.com>
2023-03-15 17:48:28 -0700

    updating a few files added like mutations queries to client

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,22 +25,15 @@ const typeDefs = `#graphql
     Books: [Book]
   }
 
-  type Checkout {
-    session: ID
-  }
-
   type Auth {
     token: ID
     user: User
   }
 
   type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
+    books(category: ID, name: String): [BOOK]
+    book(_id: ID!): BOOK
     user: User
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
   }
 
   type Mutation {
