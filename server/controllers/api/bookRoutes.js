dc1e92c1d4b81946880b07823ab3e55b0ed94cf6
Colin Marshall <colin.marshall94@gmail.com>
2023-03-16 23:27:08 -0700

    working on filtering out non-english book titles

--- server/controllers/api/bookRoutes.js
+++ server/controllers/api/bookRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 // package that allows for fetch to be used in Node
 const fetch = require("node-fetch");
-const bookTitleStrToURL = require("../../utils/apiHelpers");
+const { bookTitleStrToURL, regexGen } = require("../../utils/apiHelpers");
 // any routing related to fetching books will go here
 
 /******  START PSEUDOCODE  ******/
@@ -47,21 +47,49 @@ reference: https://openlibrary.org/dev/docs/api/books
 // })
 
 async function tempUntilServerRuns() {
-    try {
-        const bookTitle = "lord of the rings"
-        const searchTitle = bookTitleStrToURL(bookTitle)
-        // returns an array of books that match the searched title
-        const titleData = await fetch(`https://openlibrary.org/search.json?title=${searchTitle}`)
-        const titleRes = await titleData.json()
-        // gets the isbn of the first matching book - can refine this later after getting MVP
-        const isbn = titleRes.docs[0].isbn[0]
-        const bookData = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`)
-        const bookRes = await bookData.json()
-        console.log(bookRes)
-        // send this data to the database
-    } catch (err) {
-        console.log(err)
+  try {
+    const bookTitle = "lord of the rings";
+    const {regex, charLimit} = regexGen(bookTitle)
+    console.log(regex, charLimit)
+    const searchTitle = bookTitleStrToURL(bookTitle);
+    // returns an array of books that match the searched title
+    const titleData = await fetch(
+      `https://openlibrary.org/search.json?title=${searchTitle}`
+    );
+    const titleRes = await titleData.json();
+    const isbnArr = titleRes.docs[0].isbn;
+
+    // removes all indexes after 20 - eliminates edge case where page would hang due to iterating through hundreds of array indexes
+    isbnArr.splice(20, Infinity);
+
+    let i = 0;
+    while (i < isbnArr.length) {
+      const bookData = await fetch(
+        `https://openlibrary.org/api/books?bibkeys=ISBN:${isbnArr[i]}&format=json&jscmd=data`
+      );
+      const bookRes = await bookData.json();
+      // dynamically creates object key to match what is returned by Open Library
+      const isbn = Object.keys(bookRes);
+      console.log(bookRes[isbn].title.toLowerCase());
+      // if user-provided title matches 'regex' and is <= 'charLimit', use that index
+
+      // iterator if index did not match
+      i++;
     }
+
+    if (i === isbnArr.length) {
+      console.log("No book found by that title!");
+    }
+
+    // gets the isbn of the first matching book - can refine this later after getting MVP
+    // const isbn = titleRes.docs[0].isbn[0]
+    // const bookData = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`)
+    // const bookRes = await bookData.json()
+    // console.log(bookRes)
+    // send this data to the database
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-tempUntilServerRuns()
+tempUntilServerRuns();
