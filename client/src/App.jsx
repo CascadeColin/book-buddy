3c91de0e77a891af6af7acdd63d9c421c994617f
Claire Bain <claire.bain3@gmail.com>
2023-03-16 19:17:46 -0700

    sets up app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,74 @@
 import { test } from './style'
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Nav';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-center'>This is a Tailwind test!</h1>
-    </div>
-  )
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          {/* <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route forceRefresh={true}
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          /> */}
+        </Routes>
+      </>
+    </Router>
+    </ApolloProvider>
+  );
 }
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className='text-3xl font-bold text-center'>This is a Tailwind test!</h1>
+//     </div>
+//   )
+// }
 
 export default App;
