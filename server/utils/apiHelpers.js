7ac139f5fb26a5364a9ecdd8e10f0743652737c3
Colin Marshall <colin.marshall94@gmail.com>
2023-03-18 11:09:15 -0700

    see previous commit msg

--- server/utils/apiHelpers.js
+++ server/utils/apiHelpers.js
@@ -3,19 +3,13 @@ function bookTitleStrToURL(str) {
   return str.split(" ").join("+");
 }
 
-// iterates through array of books returned from API call to search based on title.  If user-provided title !== title property of book, iterate to the next arr index.  repeat until it finds a match, or return "book not found"
-function doesTitleMatch(arr) {
-    // move while loop function here if the logic gets bulky
-}
-
-// creates a regex to eliminate books returned in non-English titles
-// added 'charLimit' as a cheeky way to get rid of nonsense like "il signore degli anelli : trilogia / italian edition of the lord of the rings" matching "/lord|of|the|rings/" - ENGLISH ONLY!
-function regexGen(str) {
+function bookFilter(str) {
   return {
-    regex: new RegExp(str.split(" ").join("|")),
+    // splits string into array, adds word boundary to both sides of each word, then joins them with regex | operator - filters out versions by language
+    regex: new RegExp(str.split(' ').map(word => `\\b${word}\\b`).join('|'), 'gi'),
+    // added 'charLimit' as a cheeky way to get rid of nonsense like "il signore degli anelli : trilogia / italian edition of the lord of the rings" matching the regex
     charLimit: str.length+5
   }
 }
 
-regexGen('lord of the rings')
-module.exports = { bookTitleStrToURL, regexGen };
+module.exports = { bookTitleStrToURL, bookFilter };
