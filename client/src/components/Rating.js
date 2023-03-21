53d443b9b77a6112bb6b43af9a0e32d339431ef9
Nicole Cohen <nicolesoccer9@comcast.net>
2023-03-20 22:16:47 -0700

    rating stars addition - still having problems

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import Modal from './Modal';
+import { Rating } from 'react-simple-star-rating';
 
 
-export default function Rating() {
+export default function RatingModal() {
     const button = 'Rate Me!'
     const ratingTitle = 'Rate This Book:'
     const rateIt = 'Rate It!'
     const ratingInfo = () => {
-        
+        // const [rating, setRating] = useState(0)
+
+        // // Catch Rating value
+        // const handleRating = (rate: number) => {
+        //   setRating(rate)
+      
+        //   // other logic
+        // }
+        // // Optinal callback functions
+        // const onPointerEnter = () => console.log('Enter')
+        // const onPointerLeave = () => console.log('Leave')
+        // const onPointerMove = (value: number, index: number) => console.log(value, index)
+      
+        // return (
+        //   <div className='App'>
+        //     <Rating
+        //       onClick={handleRating}
+        //       onPointerEnter={onPointerEnter}
+        //       onPointerLeave={onPointerLeave}
+        //       onPointerMove={onPointerMove}
+        //       /* Available Props */
+        //     />
+        //   </div>
+        // )
     }
     const saveRating = () => {
 
