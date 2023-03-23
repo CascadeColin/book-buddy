cb738c0e077a7ce07e84c28b6415461426aba84d
Colin Marshall <colin.marshall94@gmail.com>
2023-03-23 15:48:30 -0700

    fixed book rating update

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,85 +1,103 @@
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import Modal from './Modal';
-import { Rating } from 'react-simple-star-rating';
-import {UPDATE_BOOK_RATING} from "../utils/mutations";
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import Modal from "./Modal";
+import { Rating } from "react-simple-star-rating";
+import { UPDATE_BOOK_RATING } from "../utils/mutations";
+import Auth from '../utils/auth'
+
+export default function RatingModal(props) {
+  console.log("props: ",props)
+  const button = "Rate Me!";
+  const ratingTitle = "Rate This Book:";
+  const rateIt = "Rate It!";
+
+  const [updateBookRating, { bookRatingError }] = useMutation(UPDATE_BOOK_RATING);
+  // rating state has information
+  const [rating, setRating] = useState(0);
 
 
-export default function RatingModal() {
-    const button = 'Rate Me!'
-    const ratingTitle = 'Rate This Book:'
-    const rateIt = 'Rate It!'
+  const activeUser = Auth.getProfile()
 
-    // rating state has information
-    const [rating, setRating] = useState(0)
-  
-        const handleRating = (rate) => {
-          setRating(rate)
-        }
-        const onPointerMove = (value, index) => console.log(value, index)
 
-        const styles = {
-          display: 'flex',
-          flexDirection: 'row'
-        }
-      
-    const saveRating = () => {
+  const handleRating = async (value, index) => {
+    console.log("handlerating value: ",value)
+    setRating(value);
+  };
+  const onPointerMove = (value, index) => /*console.log(value, index)*/ {};
 
-    }
-    return(
-        <>
-            <Modal
-                buttonName={button}
-                modalTitle={ratingTitle}
-                modalFunction={rateIt}
-                modalInformation={() => (
-                <div 
-                style={{
-                  display: 'inline'
-                }}
-                >
-                  <Rating
-                    onClick={handleRating}
-                    onPointerMove={onPointerMove}
-                    // style={styles}
-                  />
-                </div>
-                )}
-                onClickInfo={saveRating}
-                />
-        </>
-    )
+  const styles = {
+    display: "flex",
+    flexDirection: "row",
+  };
+
+  const saveRating = async () => {
+    console.log("bookId", props.book._id)
+    console.log("rating", rating)
+    const a = await updateBookRating({
+      variables: {
+        bookId: props.book._id,
+        bookRating: rating
+      }
+    })
+    console.log("new rating: ", a)
+    // window.location.reload();
+  };
+
+  return (
+    <>
+      <Modal
+        buttonName={button}
+        modalTitle={ratingTitle}
+        modalFunction={rateIt}
+        modalInformation={() => (
+          <div
+            style={{
+              display: "inline",
+            }}
+          >
+            <Rating
+              onClick={handleRating}
+              onPointerMove={onPointerMove}
+              // style={styles}
+            />
+          </div>
+        )}
+        onClickInfo={saveRating}
+      />
+    </>
+  );
 }
 
 export function BookRating() {
-   // rating state has information
-   const [rating, setRating] = useState(0)
-  
-   const handleRating = (rate) => {
-     setRating(rate)
-   }
-   const onPointerMove = (value, index) => console.log(value, index)
+  // rating state has information
+  const [rating, setRating] = useState(0);
 
-   const styles = {
-     display: 'flex',
-     flexDirection: 'row'
-   }
+  const handleRating = (value, index) => {
+   
+    setRating(index);
+  };
+  const onPointerMove = (value, index) => /*console.log(value, index);*/ {}
 
-   return(
-      <div
-        style={{
-          direction: 'ltr',
-          fontFamily: 'sans-serif',
-          touchAction: 'none'
-        }}
-        >
-          {/* read only rating stars */}
-        <Rating
-          initialValue={3}
-          onClick={handleRating}
-          onPointerMove={onPointerMove}
-          readonly
-        />
-      </div>
-   )
+  const styles = {
+    display: "flex",
+    flexDirection: "row",
+  };
+
+  return (
+    <div
+      style={{
+        direction: "ltr",
+        fontFamily: "sans-serif",
+        touchAction: "none",
+      }}
+    >
+      {/* read only rating stars */}
+      <Rating
+        initialValue={3}
+        onClick={handleRating}
+        onPointerMove={onPointerMove}
+        readonly
+      />
+    </div>
+  );
 }
