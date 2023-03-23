ca984a72dc99e19c0442f9b4d0d63c06aea57434
Kim Van Wyk <kmvanwyk@msn.com>
2023-03-22 21:57:16 -0700

    added plant to login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,6 +5,7 @@ import { useMutation } from "@apollo/client";
 //import { Link } from "react-router-dom";
 import { LOGIN } from "../../utils/mutations";
 import Auth from "../../utils/auth";
+import {LoginPlant} from "../Images";
 
 const styles = {
   main: {
@@ -114,6 +115,9 @@ function Login(props) {
           </div>
         </div>
       </form>
+      <div>
+        <LoginPlant/>
+        </div>
     </div>
   );
 }
